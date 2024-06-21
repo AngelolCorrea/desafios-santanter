41a53d27c57f4cb6b3a872341df70ce586c20bdc
Angelo Correa <angeloicloudnovo@outlook.com>
2024-06-21 11:19:24 -0300

    Desafio finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Double saldo = 257.50;
+
+        System.out.println("Informe seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Informe sua agência: ");
+        int agencia = scanner.nextInt();
+        System.out.println("Informe o numero da sua conta: ");
+        String numeroConta = scanner.next();
+
+        System.out.println("Olá: "+nome+ ". Obrigado por criar conta em nosso banco. Sua agência é: "+agencia +". Sua conta é: "+numeroConta +". Seu saldo é: "+saldo);
+
+
 
     }
 }
